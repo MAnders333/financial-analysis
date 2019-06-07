190a4e7b437ab2b7adc07a7bef999da0150a6a7e
MAnders333 <marc_anders@gmx.net>
2019-06-07 11:31:50 +0200

    Initial raw version: adds update Excel file logic

--- financials.py
+++ financials.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import openpyxl
 from datetime import datetime
 from selenium import webdriver
@@ -6,17 +7,30 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from bs4 import BeautifulSoup as soup
 
 class FinancialAnalyzer:
     """
+    This class represents an object that checks whether a given Excel file for a monthly financial
+    analysis is up to date. If no Excel file exists, it creates one in the current directory. 
+
+    :param path: path to an existing Excel file for the monthly analysis. If no Excel file exists, do not specify path and an Excel file will be created.
+    :param sheet_name: name of the sheet in the Excel file used for the monthly financial analysis. 
+    :type path: string
+    :type sheet_name: string
     """
     def __init__(self, path=None, sheet_name="Financial Overview"):
         """
+        Initializes the FinancialAnalyzer object. During initializing, it checks whether the Excel file is up to date.
         """
+        # Sets name for the sheet
         self.sheet_name = sheet_name
+
+        # Checks whether a path is specified
         if path != None:
+            # If a path is specified, path attribute is set
             self.path = str(path)
+            
+            # Tries to open Excel file and worksheet
             try:
                 self.book = openpyxl.load_workbook(self.path)
             except FileNotFoundError:
@@ -25,6 +39,8 @@ class FinancialAnalyzer:
                 self.sheet = self.book[sheet_name]
             except:
                 print("Worksheet could not be found. Please provide a valid sheet name.")
+            
+            # Checks whether worksheet is up to date
             self.last_row = self.sheet.max_row
             self.current_month = datetime.date(datetime.now()).month
             if int(self.sheet.cell(self.last_row,2).value) != int(self.current_month):
@@ -34,6 +50,7 @@ class FinancialAnalyzer:
                 self.isUpdated = True
                 print("Worksheet is up to date.")  
         else:
+            # If path is not specified, a new Excel file and worksheet are created
             self.book = openpyxl.Workbook()
             print("Workbook created.")
             self.sheet = self.book.create_sheet(title=sheet_name, index=0)
@@ -42,13 +59,27 @@ class FinancialAnalyzer:
             for i, header in enumerate(headers):
                 self.sheet.cell(1, i+1).value = header
             self.book.save("monthly_budget.xlsx")
+            
+            # As a new file and sheet were created, it needs to be updated with the account balances of the current month
             self.isUpdated = False
             print("Worksheet needs to be updated.")
 
     def scrape_bank_website(self, url, username, password):
         """
-        This method needs to be adapted for different bank websites to scrape.
+        This method logs into your online banking account and scrapes the account balances from the website.
+        
+        !!!It needs to be adapted for your bank website as its layout will be different!!!
+        
+        :param url: url of your bank's login page
+        :param username: username of your online banking account
+        :param password: password of your online banking account
+        :type url: string
+        :type username: string
+        :type password: string
+
+        For security reasons, it is recommended to use environment variables for your username and password.
         """
+
         # Visits the bank website
         self.driver = webdriver.Firefox()
         self.driver.get(url)
@@ -76,9 +107,52 @@ class FinancialAnalyzer:
         # Quits browser
         self.driver.quit()
         
-    def update_workbook(self):
-        pass
+    def update_workbook(self, cash=0.0):
+        """
+        This method updates the Excel file with the current bank balances scraped from the website plus the amount of cash in your wallet.
 
+        :param cash: the amount of cash in your wallet
+        :type cash: integer / float
+        """
+        
+        # Sets cash attribute
+        self.cash = float(cash)
+
+        current_year = datetime.date(datetime.now()).year
+        bank_balance = self.balance1 + self.balance2
+        investments = 0 # Fixed rate of investments each month
+        assets_total = self.cash + self.balance1 + self.balance2 + investments
+
+        # Checks whether this is the first entry (so cash flow and other changes in assets cannot be calculated)
+        if self.last_row == 1:
+            cashflow, bank_cashflow, investments_change, assets_total_change, assets_rel_change = 0
+        else:
+            cashflow = self.cash - float(self.sheet.cell(self.last_row, 4).value)
+            bank_cashflow = bank_balance - float(self.sheet.cell(self.last_row, 6).value)
+            investments_change = investments - float(self.sheet.cell(self.last_row, 7).value)
+            assets_total_change = assets_total - float(self.sheet.cell(self.last_row, 9).value)
+            assets_rel_change = assets_total_change / float(self.sheet.cell(self.last_row, 9).value)
+        
+        # Adds new row of values into the Excel file
+        current_values = [current_year, self.current_month, self.cash, cashflow, bank_balance, bank_cashflow, investments, investments_change, assets_total, assets_total_change, assets_rel_change]
+        for i, value in enumerate(current_values):
+            self.sheet.cell(self.last_row + 1, i + 1).value = value
+        
+        # Saves Excel file
+        self.book.save(self.path)
+
+        print("Worksheet successfully updated.")
+        print(f"""
+        cash: {round(self.cash, 2)}
+        bank: {round(bank_balance, 2)}
+        investments: {round(investments, 2)}
+        cashflow: {round(cashflow, 2)}
+        bank cashflow: {round(bank_cashflow, 2)}
+
+        total assets: {round(assets_total, 2)}
+        total assets change: {round(assets_total_change, 2)}
+        total assets relative change: {round(assets_rel_change, 2) * 100}
+        """)
 
 
 
@@ -87,11 +161,15 @@ if __name__ == "__main__":
     url = os.environ.get("BANK_URL") # Add your bank's url here
     username = os.environ.get("BK_UN") # Add your bank's username here
     password = os.environ.get("BK_PW") # Add your bank's password here
-    
+    if len(sys.argv) > 1:
+        cash = sys.argv[1] # Specify your current cash balance in the terminal as an argument
+    else:
+        cash = 0.0
+
     analyzer = FinancialAnalyzer(path=path)
     if analyzer.isUpdated == False:
         analyzer.scrape_bank_website(url, username, password) 
-        analyzer.update_workbook()
+        analyzer.update_workbook(cash)
 
     
 
